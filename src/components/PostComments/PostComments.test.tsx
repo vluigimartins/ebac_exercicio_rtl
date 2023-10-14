8b74e6d3e118d2f44c50ad4eca0ba1fc145e244c
Vinicius <vini-luiz@hotmail.com>
2023-10-14 12:36:36 -0300

    Criando o teste dos comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,29 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('Deve adicionar dois comentários', () => {
+        render(<PostComment/>);
+
+        fireEvent.change(screen.getByTestId('post-comments-textarea'), {
+            target: {
+                value: 'Teste para adicionar comentário',
+            }
+        });
+        fireEvent.click(screen.getByTestId('post-comment-button'));
+    
+        fireEvent.change(screen.getByTestId('post-comments-textarea'), {
+            target: {
+                value: 'Teste para adicionar comentário 2',
+            }
+        });
+        fireEvent.click(screen.getByTestId('post-comment-button'));
+
+        expect(screen.getAllByTestId('comment-element')).toHaveLength(2);
     });
 });
